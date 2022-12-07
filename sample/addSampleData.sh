356d418c5aa86b7bf52a1db8876c0c3f250e71ca
mauiroma <maurizio.romani@gmail.com>
2022-12-07 21:26:05 +0100

    Add Hack to bypass file location issue

--- sample/addSampleData.sh
+++ sample/addSampleData.sh
@@ -1,6 +1,6 @@
 sleep 5
 echo "Add Sample Data"
-cat 
+cat /sample/sample.ldif
 ldapadd -x -D "cn=Manager,dc=example,dc=com" -f /sample/sample.ldif -w admin
 echo "verify"
 ldapsearch -x -b "dc=example,dc=com" -H ldap://localhost -D "cn=Manager,dc=example,dc=com" -w admin "(objectClass=person)"
